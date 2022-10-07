56a6bfce4b03d05a817ab2690e1fccd6d15eeef7
Meetsimar <meetsimark@gmail.com>
2022-10-07 13:27:42 -0400

    initial commit

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
+/*************************************************************************
+* BTI325– Assignment 2
+* I declare that this assignment is my own work in accordance with Seneca Academic
+Policy. No part * of this assignment has been copied manually or electronically from any
+other source
+* (including 3rd party web sites) or distributed to other students.
+*
+* Name: Meetsimar Kaur Student ID: 106510217 Date: 07/10/2022
+*
+* Your app’s URL (from Cyclic) : ______________________________________________
+*
+*************************************************************************/
+
 var express = require("express");
 var app = express();
 
